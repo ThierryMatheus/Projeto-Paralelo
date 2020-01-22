0e652e7de8642c42fd9f1973189e5d6a74d52a4a
ThierryMatheus <49615769+ThierryMatheus@users.noreply.github.com>
2020-01-22 02:13:59 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,28 +2,41 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-const players = {}
+const Players = {}
+
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/adedonha.html');
 });
 
+
 io.on('connection', function(socket){
-	players[socket.id] = {
-		nome:'Nome',
-		points: 0,
+	Players[socket.id] = {
+		nome:"Thierry",
+		points:0
 	}
-  console.log(socket.id +' connected');
-  socket.on('disconnect', function(){
+	socket.on('nome',name=>{
+		Players[socket.id].nome=name;
+		})
+
+	console.log(socket.id +' connected');
+
+
+  socket.on('disconnect', function(data){
+    delete Players[socket.id]
+    socket.broadcast.emit('Players',Players)
     console.log('user disconnected');
   })
+    socket.broadcast.emit('Players',Players)
+
 // socket.on('nome', name => {
-//	players[socket.id].nome = name;
+//	Players[socket.id].nome = name;
 //	console.log(name);
 //  })
-	socket.broadcast.emit('hi');
+//	socket.broadcast.emit('hi');
 });
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
+	http.listen(3000, function(){
+	console.log('listening on *:3000');
+})
